5f464c5f35e3040814ef1b7a2960e4cf8fa7b289
netrometro <netrometro@yahoo.com.br>
2019-09-02 10:52:22 -0300

    Correção da configuração do PrimeFlex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { ButtonModule } from 'primeng/button';
+
 import { AppComponent } from './app.component';
 import { MenubarComponent } from './menubar/menubar.component';
 
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, ButtonModule ],
   declarations: [ AppComponent, MenubarComponent ],
   bootstrap:    [ AppComponent ]
 })
