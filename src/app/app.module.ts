a55e62c4fe08bb620383200e1c5d96feb03dd8d3
netrometro <netrometro@yahoo.com.br>
2019-09-02 10:38:51 -0300

    index.html e css do body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MenubarComponent } from './menubar/menubar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, MenubarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
