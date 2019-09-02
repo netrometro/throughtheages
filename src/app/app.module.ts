07cbff55bda7bae6f6301ac0e35e8867510218c4
netrometro <netrometro@yahoo.com.br>
2019-09-02 11:40:34 -0300

    Acoordion Timeline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ButtonModule } from 'primeng/button';
+import { AccordionModule } from 'primeng/accordion';
 
 import { AppComponent } from './app.component';
 import { MenubarComponent } from './menubar/menubar.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ButtonModule ],
-  declarations: [ AppComponent, MenubarComponent ],
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, ButtonModule, AccordionModule ],
+  declarations: [ AppComponent, MenubarComponent, TimelineComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
