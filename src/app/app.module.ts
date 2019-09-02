8e57258e9cfef58753e71e9df4a3d243c029065d
netrometro <netrometro@yahoo.com.br>
2019-09-02 13:38:25 -0300

    Scroling timeline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ButtonModule } from 'primeng/button';
+import { DragDropModule } from 'primeng/dragdrop';
 import { AccordionModule } from 'primeng/accordion';
 
 import { AppComponent } from './app.component';
 import { MenubarComponent } from './menubar/menubar.component';
 import { TimelineComponent } from './timeline/timeline.component';
+import { CardComponent } from './card/card.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, ButtonModule, AccordionModule ],
-  declarations: [ AppComponent, MenubarComponent, TimelineComponent ],
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, ButtonModule, AccordionModule, DragDropModule ],
+  declarations: [ AppComponent, MenubarComponent, TimelineComponent, CardComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
